f3b2a205329c660c056b3ce9d4a44b169c9b3b3f
Imam Ramadhan <imamrmdn99@gmail.com>
2022-11-03 09:39:14 +0700

    modified : password hash and avoid sql injection

--- functions/user.php
+++ functions/user.php
@@ -4,6 +4,15 @@ function register_user($firstname, $lastname, $email, $password)
 {
     global $link;
 
+    //avoid sql injection
+    $firstname  = mysqli_real_escape_string($link, $firstname);
+    $lastname   = mysqli_real_escape_string($link, $lastname);
+    $email      = mysqli_real_escape_string($link, $email); 
+    $password   = mysqli_real_escape_string($link, $password);
+
+    //password hash
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
     $query = "INSERT INTO users (firstname, lastname, email, password) VALUES ('$firstname', '$lastname', '$email', '$password')";
 
     if (mysqli_query($link, $query)) {
